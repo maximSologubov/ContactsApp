b0f2352c5e0ae1984184de7b020181de52a5a062
Maxim Sologubov <maximSologubov@gmail.com>
2021-12-16 15:48:08 +0200

    закомментил половину кода исчезла ошибка

--- Contact App/Contact App/App.xaml.cs
+++ Contact App/Contact App/App.xaml.cs
@@ -1,21 +1,28 @@
-﻿using Prism;
+﻿using Contact_App.Services.Settings;
+using Contact_App.ViewModels;
+using Contact_App.Views;
+using Prism;
 using Prism.Ioc;
 using Prism.Unity;
 using System;
+using System.Collections.Generic;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-
 namespace Contact_App
 {
     public partial class App : PrismApplication
     {
+        
         public App(IPlatformInitializer initializer = null) : base(initializer) 
         {
-            InitializeComponent();
 
-            MainPage = new MainPage();
         }
+        #region --- Private fields
+        private ISettinngsManager _settinngsManager;
 
+        #endregion       
+
+        #region --- Overrides ---
         protected override void OnStart()
         {
         }
@@ -30,12 +37,26 @@ namespace Contact_App
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
-            //throw new NotImplementedException();
+            //Services
+
+            containerRegistry.RegisterInstance<ISettinngsManager>(Container.Resolve<SettingsManager>());
+            //Navigation
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<SignInPage, SignInPageViewModel>();
+            containerRegistry.RegisterForNavigation<SignUpPage, SignUpPageViewModel>();
+            //containerRegistry.RegisterForNavigation<MainListView, MainListViewModel>();
         }
 
         protected override void OnInitialized()
         {
-            //throw new NotImplementedException();
+            InitializeComponent();
+
+            NavigationService.NavigateAsync($"{nameof(SignUpPage)}");
+          
+            
+
         }
+        #endregion
+
     }
 }
